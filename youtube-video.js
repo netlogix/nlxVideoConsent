d756a1adec3f763a998bec7d484e1c475e40fc68
Julius Noack <julius.noack@netlogix.de>
2022-09-28 10:19:06 +0200

    add dark mode for darker designs or bright backgrounds

--- youtube-video.js
+++ youtube-video.js
@@ -67,8 +67,12 @@ class YoutubeVideo extends HTMLElement {
         return autoplayOnConfirm === 'true';
     }
 
+    get darkMode() {
+        return this.getAttribute("darkMode") === 'true';
+    }
+
     static get observedAttributes() {
-        return ['src', 'autoplay', 'alt', 'aspectRatio', 'autoplayOnConfirm', 'textOrientation', 'textSize', 'showIcon'];
+        return ['src', 'autoplay', 'alt', 'aspectRatio', 'autoplayOnConfirm', 'textOrientation', 'textSize', 'showIcon', 'darkMode'];
     }
 
     connectedCallback() {
@@ -164,11 +168,12 @@ class YoutubeVideo extends HTMLElement {
                 youtube-video span {
                     font-size: ${this.textSize}rem;
                     margin-left: 0.3rem;
-                    color: #fff;
+                    color: ${this.darkMode ? '#000': '#fff'};
                 }
 
                 youtube-video svg {
                     display: ${this.showIcon ? 'block' : 'none'};
+                    fill: ${this.darkMode ? '#000': '#fff'};
                 }
             </style>
         `;
@@ -177,7 +182,7 @@ class YoutubeVideo extends HTMLElement {
     get notConfirmedHtml() {
         return `
             <div class="nlx-video-container" onClick="this.parentElement.confirmConsent()">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="#fff" width="48" height="48">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="48" height="48">
                     <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path>
                 </svg>
                 <span>${this.alt}</span>
