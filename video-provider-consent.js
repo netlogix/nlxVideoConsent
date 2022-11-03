24098ffaf431e741fc3b0bd2fcf8410fa1dea8c0
Julius Noack <julius.noack@netlogix.de>
2022-11-03 17:28:15 +0100

    change element to use whole size of parent element

--- video-provider-consent.js
+++ video-provider-consent.js
@@ -207,7 +207,7 @@ class VideoProviderConsent extends HTMLElement {
                     aspect-ratio: ${this.aspectRatio};
                 }
 
-                .nlx-video-container {
+                .nlx-video-controls {
                     display: flex;
                     position: absolute;
                     left: 50%;
@@ -215,6 +215,12 @@ class VideoProviderConsent extends HTMLElement {
                     transform: translate(-50%, -50%);
                     align-items: center;
                     flex-direction: ${this.textOrientation};
+                }
+
+                .nlx-video-container,
+                .nlx-video-container iframe {
+                    height: 100%;
+                    width: 100%;
                 }
 
                 video-provider-consent span {
@@ -234,20 +240,22 @@ class VideoProviderConsent extends HTMLElement {
 
     get notConfirmedHtml() {
         return `
-            <div class="nlx-video-container" onClick="this.parentElement.confirmConsent()">
+        <div class="nlx-video-container" onClick="this.parentElement.confirmConsent()">
+            <div class="nlx-video-controls">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="48" height="48">
                     <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path>
                 </svg>
                 <span>${this.text}</span>
             </div>
+        </div>
         `;
     }
 
     get confirmedHtml() {
         return `
-            <div class="nlx-video-container" onClick="this.parentElement.confirmConsent()">
-                <iframe width="560" height="315" />
-            </div>
+        <div class="nlx-video-container" onClick="this.parentElement.confirmConsent()">
+            <iframe width="560" height="315" />
+        </div>
         `;
     }
 }
